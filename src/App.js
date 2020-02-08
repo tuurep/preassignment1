5f7b6243ca387abcfda2d95e1d44e795a0165afe
tuure <tuure.piitulainen@gmail.com>
2020-02-08 20:17:36 +0200

    Fetch data through graphql

--- src/App.js
+++ src/App.js
@@ -1,28 +1,84 @@
 import React from 'react';
 import './App.css';
+import ApolloClient from 'apollo-boost';
+import { ApolloProvider, Query } from 'react-apollo';
+import gql from 'graphql-tag';
+import Moment from 'react-moment';
+
+Moment.globalFormat = 'HH:mm';
+
+const client = new ApolloClient({
+  uri: 'https://api.digitransit.fi/routing/v1/routers/hsl/index/graphql',
+})
+
+const kumpula_coordinates = {
+  lat: 60.2051,
+  lon: 24.9623
+}
+
+const eficode_coordinates = {
+  lat: 60.169433,
+  lon: 24.925835
+}
+
+const PLAN_QUERY = gql`
+  {
+    plan(
+      from: {lat: ${kumpula_coordinates.lat}, lon: ${kumpula_coordinates.lon}}
+      to: {lat: ${eficode_coordinates.lat}, lon: ${eficode_coordinates.lon}}
+      numItineraries: 6
+    ) {
+      itineraries {
+        startTime
+        legs {
+          mode
+          route {
+            shortName  
+          }
+          from {
+            name
+          }
+          to {
+            name
+          }
+        }
+      endTime
+      duration
+      }
+    }
+  }
+`
 
 function App() {
   return (
-    <div>
-        <p>
-          Pohjoinen Rautatiekatu 25 - Kumpulan kampus
-        </p>
+    <ApolloProvider client={client}>
+      <div>
+        <p>Kumpula - Eficode HQ</p>
+
         <ul>
-          <li>15:53 ratikka 8</li>
-          <li>15:53 bussi 70</li>
-          <li>16:03 ratikka 8</li>
-          <li>16:03 bussi 70</li>
+          <Query query={PLAN_QUERY}>
+            {({ loading, data }) => {
+              if (loading) return 'Loading...'
+              const itineraries = data.plan.itineraries
+              return (
+                <div>
+                  {itineraries.map(i => 
+                    <div>
+                      <h3>Start time: <Moment unix>{i.startTime}</Moment></h3>
+                      <ul>
+                        {i.legs.map(l => 
+                          <li>{l.mode} {l.from.name} - {l.to.name}</li>
+                        )}
+                      </ul>
+                    </div>
+                  )}
+                </div>
+              )
+            }}
+          </Query>
         </ul>
-        <p>
-          Kumpulan kampus - Pohjoinen Rautatiekatu 25
-        </p>
-        <ul>
-          <li>15:51 ratikka 8</li>
-          <li>15:56 ratikka 6T - ratikka 1</li>
-          <li>15:59 bussi 70</li>
-          <li>16:00 ratikka 8</li>
-        </ul>
-    </div>
+      </div>
+    </ApolloProvider>
   );
 }
 
