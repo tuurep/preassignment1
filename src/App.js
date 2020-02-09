9ade71378462e4015b99090fda3e70900ef8d183
tuure <tuure.piitulainen@gmail.com>
2020-02-09 15:07:25 +0200

    Refactor query into reusable component

--- src/App.js
+++ src/App.js
@@ -48,48 +48,66 @@ const PLAN_QUERY = gql`
   }
 `
 
+const Plan = ({ from_lat, from_lon, to_lat, to_lon }) => (
+  <Query
+    query={PLAN_QUERY}
+    variables={{
+      from_lat: from_lat,
+      from_lon: from_lon,
+      to_lat: to_lat,
+      to_lon: to_lon
+    }}
+  >
+
+    {({ loading, data }) => {
+      if (loading) return <p>Loading...</p>
+      const itineraries = data.plan.itineraries
+      return (
+        <div>
+          {itineraries.map(i => 
+            <div>
+              <h3>Start time: {moment(i.startTime).format("HH:mm")}</h3>
+              <ul>
+                {i.legs.map(l => 
+                  <div> 
+                    {l.mode !== 'WALK' &&
+                      <li>
+                        {moment(l.startTime).format("HH:mm")} {' [ '}
+                        {l.mode} {' '}
+                        {l.mode !== 'SUBWAY' && l.route.shortName} {'] '}
+                        {l.from.name} - {l.to.name}
+                      </li>
+                    }
+                  </div>
+                )}
+              </ul>
+            </div>
+          )}
+        </div>
+      )
+    }}
+  </Query>
+)
+
 function App() {
   return (
     <ApolloProvider client={client}>
       <div>
         <h2>Kumpulan kampus - Eficode HQ</h2>
-          <Query
-            query={PLAN_QUERY}
-            variables={{
-              from_lat: kumpula_coordinates.lat,
-              from_lon: kumpula_coordinates.lon,
-              to_lat: eficode_coordinates.lat,
-              to_lon: eficode_coordinates.lon}}
-            >
+        <Plan
+          from_lat={kumpula_coordinates.lat}
+          from_lon={kumpula_coordinates.lon}
+          to_lat={eficode_coordinates.lat}
+          to_lon={eficode_coordinates.lon}
+        />
 
-            {({ loading, data }) => {
-              if (loading) return <p>Loading...</p>
-              const itineraries = data.plan.itineraries
-              return (
-                <div>
-                  {itineraries.map(i => 
-                    <div>
-                      <h3>Start time: {moment(i.startTime).format("HH:mm")}</h3>
-                      <ul>
-                        {i.legs.map(l => 
-                          <div> 
-                            {l.mode !== 'WALK' &&
-                              <li>
-                                {moment(l.startTime).format("HH:mm")} {' [ '}
-                                {l.mode} {' '}
-                                {l.mode !== 'SUBWAY' && l.route.shortName} {'] '}
-                                {l.from.name} - {l.to.name}
-                              </li>
-                            }
-                          </div>
-                        )}
-                      </ul>
-                    </div>
-                  )}
-                </div>
-              )
-            }}
-          </Query>
+        <h2>Eficode HQ - Kumpulan kampus</h2>
+        <Plan
+          from_lat={eficode_coordinates.lat}
+          from_lon={eficode_coordinates.lon}
+          to_lat={kumpula_coordinates.lat}
+          to_lon={kumpula_coordinates.lon}
+        />
       </div>
     </ApolloProvider>
   );
