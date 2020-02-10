33fd136f04306bffe57b70d4e10b905268309021
tuure <tuure.piitulainen@gmail.com>
2020-02-10 19:19:37 +0200

    Add arrival time & boxes

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ import metro_icon from './icons/metro.svg';
 import tram_icon from './icons/tram.svg';
 
 const client = new ApolloClient({
-  uri: 'https://api.digitransit.fi/routing/v1/routers/hsl/index/graphql',
+  uri: 'https://api.digitransit.fi/routing/v1/routers/hsl/index/graphql'
 })
 
 const kumpula_coordinates = {
@@ -55,8 +55,17 @@ const styles = {
   Paper: {
     padding: 60,
     paddingBottom: 70,
-    margin: 30,
+    margin: 60,
     backgroundColor: '#f5f5f5'
+  },
+  AlignIconWithText: {
+    verticalAlign: 'sub'
+  },
+  Bordered: {
+    border: 'solid',
+    borderColor: '#757575',
+    backgroundColor: '#eeeeee',
+    padding: 10
   }
 }
 
@@ -84,39 +93,44 @@ const Plan = ({ from_lat, from_lon, to_lat, to_lon }) => (
           {itineraries.map(i => 
             <Grid
               container direction="row"
-              justify="flex-start"
-              alignItems="flex-start"  
+              justify="space-between"
+              alignItems="center"  
               spacing={6}
             >
-              <Grid item>
+              <Grid item xs={2}>
                 <h2>{moment(i.startTime).format("HH:mm")}</h2>
               </Grid>
-              <Grid item>
-                {i.legs.map(l => 
-                  <div> 
-                    {l.mode !== 'WALK' &&
-                      <div>
-                        {l.mode === 'BUS' && 
-                          <div>
-                            <img src={bus_icon} alt="BUS" style={{ verticalAlign: "sub" }}/> {' '}
-                            <font size="5.5" color="#007AC9">{l.route.shortName}</font>
+              <Grid item xs>
+                <div style={styles.Bordered}>
+                  {i.legs.map(l => 
+                    <div> 
+                      {l.mode !== 'WALK' &&
+                        <div>
+                          {l.mode === 'BUS' && 
+                            <div>
+                              <img src={bus_icon} alt="BUS" style={styles.AlignIconWithText} /> {' '}
+                              <font size="5.5" color="#007AC9">{l.route.shortName}</font>
+                            </div>
+                          }
+                          {l.mode === 'TRAM' && 
+                            <div>
+                              <img src={tram_icon} alt="TRAM" style={styles.AlignIconWithText} /> {' '}
+                              <font size="5.5" color="#00985F">{l.route.shortName}</font>
+                            </div>
+                          }
+                          {l.mode === 'SUBWAY' && <img src={metro_icon} alt="METRO" />} {' '}
+                          <div style={{ marginTop: 8, marginBottom: 15 }}>
+                            <b>{moment(l.startTime).format("HH:mm")} </b> {' '}
+                            {l.from.name} &#10140; {l.to.name}
                           </div>
-                        }
-                        {l.mode === 'TRAM' && 
-                          <div>
-                            <img src={tram_icon} alt="TRAM" style={{ verticalAlign: "sub" }} /> {' '}
-                            <font size="5.5" color="#00985F">{l.route.shortName}</font>
-                          </div>
-                        }
-                        {l.mode === 'SUBWAY' && <img src={metro_icon} alt="METRO" />} {' '}
-                        <div style={{ marginTop: 8, marginBottom: 15 }}>
-                          <b>{moment(l.startTime).format("HH:mm")} </b> {' '}
-                          {l.from.name} &#10140; {l.to.name}
                         </div>
-                      </div>
-                    }
-                  </div>
-                )}
+                      }
+                    </div>
+                  )}
+                </div>
+              </Grid>
+              <Grid item xs={2}>
+                <h2>{moment(i.endTime).format("HH:mm")}</h2>
               </Grid>
             </Grid>
           )}
@@ -130,14 +144,16 @@ function App() {
   return (
     <Grid container 
       direction="row"
-      justify="space-between"
+      justify="space-evenly"
       alignItems="flex-start"
       spacing={10}
     >
       <ApolloProvider client={client}>
-        <Grid item md>
+        <Grid item lg>
           <Paper elevation={4} style={styles.Paper}>
-            <h1>Kumpulan kampus &#10140; Eficode HQ</h1>
+            <h1 style={{ textAlign: 'center' }}>
+              Kumpulan kampus &#10140; Eficode HQ
+            </h1>
             <Plan
               from_lat={kumpula_coordinates.lat}
               from_lon={kumpula_coordinates.lon}
@@ -146,9 +162,11 @@ function App() {
             />
           </Paper>
         </Grid>
-        <Grid item md>
+        <Grid item lg>
           <Paper elevation={4} style={styles.Paper}>
-            <h1>Eficode HQ &#10140; Kumpulan kampus</h1>
+            <h1 style={{ textAlign: 'center' }}>
+              Eficode HQ &#10140; Kumpulan kampus
+            </h1>
             <Plan
               from_lat={eficode_coordinates.lat}
               from_lon={eficode_coordinates.lon}
